02823aac612609b5f8776ff5a20b5deef0868642
hgsant <santoslucashugo@gmail.com>
2025-11-13 00:16:28 -0300

    aaa

--- acadia_code/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Requer gcc (MinGW) ou cl (MSVC) no PATH.
-gcc -shared -o c_module.dll c_module.c
-echo Gerado: c_module.dll
--- acadia_code/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-gcc -shared -fPIC c_module.c -o libc_module.so
-echo "Gerado: libc_module.so"
